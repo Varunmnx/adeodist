0b8e1833add9e5183a6c20496baac6560ad81d04
varunmnx <varun.cheriyath@gmail.com>
2023-09-28 15:46:07 +0530

    disabled loggin

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const Sequelize = require("sequelize");
 const dbConfig = require("./db.config.js");
 
-console.log(dbConfig);
 const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   port: dbConfig.port,
   operatorsAliases: false,
+  logging: false,
 });
 
 module.exports = sequelize;
